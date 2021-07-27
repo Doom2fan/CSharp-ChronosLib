aae6fb34c3ef0e59ad447c070dbe8bb92d4130b1
Chronos Ouroboros <doom2.fan@gmail.com>
2021-07-27 17:26:31 -0300

    Added some extensions for getting pooled strings.

--- Pooled/PooledUtilities.cs
+++ Pooled/PooledUtilities.cs
@@ -10,6 +10,9 @@
 #nullable enable
 
 using System;
+using System.Runtime.CompilerServices;
+using System.Text;
+using CommunityToolkit.HighPerformance.Buffers;
 
 namespace ChronosLib.Pooled {
     public static class PooledUtils {
@@ -24,5 +27,94 @@ namespace ChronosLib.Pooled {
             if (!(default (T) == null) && value == null)
                 throw new ArgumentNullException (paramName);
         }
+
+        #region Span<char> to string
+
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
+        public static string GetPooledString (this Span<char> chars) {
+            return StringPool.Shared.GetOrAdd (chars);
+        }
+
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
+        public static string GetPooledString (this ReadOnlySpan<char> chars) {
+            return StringPool.Shared.GetOrAdd (chars);
+        }
+
+        #endregion
+
+        #region Span<Rune> to string
+
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
+        public static string GetPooledString (this Span<Rune> runes) {
+            using var chars = runes.GetPooledChars ();
+            return StringPool.Shared.GetOrAdd (chars.Span);
+        }
+
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
+        public static string GetPooledString (this ReadOnlySpan<Rune> runes) {
+            using var chars = runes.GetPooledChars ();
+            return StringPool.Shared.GetOrAdd (chars.Span);
+        }
+
+        #endregion
+
+        #region Span<char> to string - lowercase, invariant culture
+
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
+        public static string GetPooledString_LowerInvariant (this Span<char> str) {
+            return ((ReadOnlySpan<char>) str).GetPooledString_LowerInvariant ();
+        }
+
+        public static string GetPooledString_LowerInvariant (this ReadOnlySpan<char> str) {
+            // Calculate the total length after turning it lowercase. Shouldn't change, but lol Unicode so there's no guarantees.
+            // We also check if we even need to do it in the first place.
+            int totalLen = 0;
+            bool anyChanged = false;
+            foreach (var rune in str.EnumerateRunes ()) {
+                var runeLower = Rune.ToLowerInvariant (rune);
+                totalLen += runeLower.Utf16SequenceLength;
+
+                anyChanged |= runeLower != rune;
+            }
+
+            if (!anyChanged)
+                return str.GetPooledString ();
+
+            // Make the CVar name lowercase.
+            using var strChars = PooledArray<char>.GetArray (totalLen);
+            var strCharsSpan = strChars.Span;
+            foreach (var rune in str.EnumerateRunes ()) {
+                int len = Rune.ToLowerInvariant (rune).EncodeToUtf16 (strCharsSpan);
+                strCharsSpan = strCharsSpan.Slice (len);
+            }
+
+            return strChars.Span.GetPooledString ();
+        }
+
+        #endregion
+
+        #region Span<Rune> to char array
+
+        public static PooledArray<char> GetPooledChars (this ReadOnlySpan<Rune> runes) {
+            int totalLen = 0;
+            foreach (var rune in runes)
+                totalLen += rune.Utf16SequenceLength;
+
+            var ret = PooledArray<char>.GetArray (totalLen);
+            var retSpan = ret.Span;
+            foreach (var rune in runes) {
+                int len = rune.EncodeToUtf16 (retSpan);
+                retSpan = retSpan.Slice (len);
+            }
+
+            return ret;
+        }
+
+        [MethodImpl (MethodImplOptions.AggressiveInlining)]
+        public static PooledArray<char> GetPooledChars (this Span<Rune> runes) {
+            return ((ReadOnlySpan<Rune>) runes).GetPooledChars ();
+        }
+
+        #endregion
     }
 }
