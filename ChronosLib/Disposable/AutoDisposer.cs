e1f59452b26fb2788749c9f28c6a44ca1169050e
Chronos Ouroboros <doom2.fan@gmail.com>
2023-07-10 01:59:49 -0300

    Add auto-disposer struct. Bump the minor version.

---
+++ ChronosLib/Disposable/AutoDisposer.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * ChronosLib - A collection of useful things
+ * Copyright (C) 2018-2020 Chronos "phantombeta" Ouroboros
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+using System;
+
+namespace ChronosLib.Disposable;
+
+public ref struct AutoDisposer<T> where T : IDisposable {
+    private ref T disposable;
+
+    public AutoDisposer (ref T val) => disposable = ref val;
+
+    public void Dispose () => disposable.Dispose ();
+}
---
+++ ChronosLib/Disposable/DisposeUtils.cs
@@ -0,0 +1,16 @@
+﻿/*
+ * ChronosLib - A collection of useful things
+ * Copyright (C) 2018-2020 Chronos "phantombeta" Ouroboros
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+using System;
+
+namespace ChronosLib.Disposable;
+
+public static class CL_DisposeUtils {
+    public static AutoDisposer<T> AutoDispose<T> (this ref T val) where T : struct, IDisposable => new (ref val);
+}
